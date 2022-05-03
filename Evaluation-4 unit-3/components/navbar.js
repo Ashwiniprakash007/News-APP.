d407ad4076829bf5bc9d2671075ebc9af4d7d1e7
Ashwini Prakash <100183151+ashwiniprkash@users.noreply.github.com>
2022-05-04 01:11:13 +0530

    Add files via upload

---
+++ Evaluation-4 unit-3/components/navbar.js
@@ -0,0 +1,7 @@
+let navbar=()=>{
+    return `<h1><a href="#">NewsApp</a></h1>
+    <div id="search_bar">
+      <input type="text" id="search_input" placeholder="Search News">
+    </div>`
+};
+export {navbar};
